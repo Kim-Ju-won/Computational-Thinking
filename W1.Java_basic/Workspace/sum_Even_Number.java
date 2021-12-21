aec2dad1e205cb444ffdbc54c0f7d57ce3a37510
김주원 <kjwt1124@icloud.com>
2021-12-22 02:20:28 +0900

    Upload Algorithm Java Basic week1 2021.12.22 02:20

---
+++ W1.Java_basic/Workspace/sum_Even_Number.java
@@ -0,0 +1,14 @@
+public class sum_Even_Number {
+    public static void main(String[] args) {
+      int hap = 0;
+      int i = 0;
+    while(i<100) {
+     i=i+1;
+     if(i%2==0)
+     {
+       hap=hap+i;
+     }
+}
+ System.out.println(i+ ","+hap);
+    }
+}
---
+++ W1.Java_basic/Workspace/sum_Odd_Number.java
@@ -0,0 +1,14 @@
+public class sum_Odd_Number {
+    public static void main(String[] args) {
+      int hap = 0;
+      int i = 0;
+    while(i<100) {
+     i=i+1;
+     if(i%2==1)
+     {
+       hap=hap+i;
+     }
+}
+ System.out.println(i+ ","+hap);
+    }
+}
