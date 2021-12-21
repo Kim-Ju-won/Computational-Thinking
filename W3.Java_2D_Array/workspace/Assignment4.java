99e7dfb1bede98ed001ba2fbbbcc8a76f610c379
김주원 <kjwt1124@icloud.com>
2021-12-22 02:34:03 +0900

    Upload Algorithm Java 2D Array week3 2021.12.22 02:34

---
+++ W3.Java_2D_Array/workspace/Assignment4.java
@@ -0,0 +1,25 @@
+public class Assignment4{
+    public static void main(String args[]) {
+        int i,j,n=0,sum=0;
+		int [][] a = new int [5][5];
+		for (i=0;i<5;i++){
+			for (j=0;j<5;j++ )	{
+				n=n+1;
+				a[i][j]=n;
+                          	System.out.printf("%5d",a[i][j]);
+			}
+			System.out.println();
+		}
+
+		i=0;
+		j=0;
+
+		for (i=0;i<5;i++){
+			for (j=0;j<=i;j++ ){
+					sum=sum+a[i][j];
+			}
+		}
+        
+		System.out.println("ÃÑ ÇÕ°è´Â"+sum);
+    }
+}
---
+++ W3.Java_2D_Array/workspace/Assignment7.java
@@ -0,0 +1,24 @@
+public class Assignment7{
+    public static void main(String args[]) {
+        int i,j,n=0,sum=0;
+		int [][] a = new int [5][5];
+		for (i=0;i<5;i++){
+			for (j=0;j<5;j++ )	{
+				n=n+1;
+				a[i][j]=n;
+                          	System.out.printf("%5d",a[i][j]);
+			}
+			System.out.println();
+		}
+
+		i=0;
+		j=0;
+
+		for (i=0;i<3;i++){
+			for (j=i;j<=4-i;j++){
+					sum=sum+a[j][i];
+			}
+		}
+		System.out.println("ÃÑ ÇÕ°è´Â"+sum);
+    }
+}
