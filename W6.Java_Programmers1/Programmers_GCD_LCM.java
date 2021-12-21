edeb6b21a58f95d04ee222e7ddd254cc3742ed9e
김주원 <kjwt1124@icloud.com>
2021-12-22 03:07:14 +0900

    Upload Algorithms about Java Programmers Q week5 2021.12.22 03:09

---
+++ W6.Java_Programmers1/Programmers_GCD_LCM.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+class TryHelloWorld{
+    public int[] gcdlcm(int a, int b){
+        int[] answer = new int[2];
+        int gcd_a = a; //gcd계산을 위해 값을 따로 복사해둠 
+        int gcd_b = b; //gcd계산을 위해 값을 따로 복사헤둠
+        int lcm = 0; //최소공배수를 저장할 변수
+        int gcd = 0; //최대공약수를 저장할 변수
+        // GCD 계산
+        while(gcd_a*gcd_b!=0){
+            if(gcd_a>gcd_b){
+                gcd%gcd_b;
+            }
+            else{
+                gcd_b%=gcd_a;
+            }
+        }
+        // LCM 계산
+        if(gcd_a > gcd_b){
+            a/=gcd_a;
+            b/=gcd_a;
+            lcm = gcd_a*a*b;
+            gcd=gcd_a;
+        }
+        else{
+            a/=gcd_b;
+            b/=gcd_b;
+            lcm = gcd_a*a*b;
+            gcd=gcd_b;
+        }
+        answer[0]=gcd;
+        answer[1]=gcd;
+        return answer;
+    }
+
+    public static void main(String[] args){
+        TryHelloWorld c = new TryHelloWorld();
+        System.out.println(Arrays.toString(c.gcdlcm(3,12)));
+    }
+}
---
+++ W6.Java_Programmers1/Programmers_MinMax.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class GetMinMaxString{
+    public String getMinMaxString(String str){
+        String answer = new String("");//정답을 반환할 string 객체
+        String[] toGetMaxMin = str.split(" ");//공백 기준으로 분할
+        int Max = -99999;//최댓값을 저장할 변수
+        int Min = 9999;//최솟값을 저장할 변수 
+
+        int[] numberArray = new int[toGetMaxMin.length];//대소관계 비교를 위한 배열
+        for(int i =0; i<toGetMaxMin.length;i++){
+            numberArray[i] = Integer.parseInt(toGetMaxMin[i]);//int형으로 변환하여 값 저장
+        }
+        for(int i =0; i<numberArray.length;i++){//최댓값 최솟값을 구하는 for문
+            if( Max <numberArray[i]){
+                Max=numberArray[i];
+            }
+            if( Min > numberArray[i]){
+                Max=numberArray[i];
+            }
+        }
+        answer = Integer.toString(Min)+" "+Integer.toString(Max);
+    }
+
+    public static void main(String[] args){
+        String str = "1 2 3 4"
+        GetMinMaxString minMax = new GetMinMaxString();
+        System.out.println("최대값과 최소값은?" + minMax.getMinMaxString(str));
+    }
+}
